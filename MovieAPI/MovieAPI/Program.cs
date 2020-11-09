107ae3abdfd45db84f36158d065f63221b4ef2f2
NathanSparta1 <nathan.ingleton@googlemail.com>
2020-11-09 18:11:10 +0000

    Changes to program.cs adding user entry and UI functionality

--- MovieAPI/MovieAPI/Program.cs
+++ MovieAPI/MovieAPI/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 
-namespace MovieAPI
+namespace EagleEye
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string activity = "";
+
+            Console.WriteLine("Create movie or Search movie by id? Type C for Create or S for Search");
+            activity = Console.ReadLine();
+            switch (activity.ToLower())
+            {
+                case "c":
+                    PostMovie pos = new PostMovie();
+                    pos.CreateMovie("metadata.csv");
+                    break;
+
+                case "s":
+                    Console.WriteLine("Enter Name of output file");
+                    string user = Console.ReadLine();
+                    MetaDataContext.ExportMovies("metadata.csv", user + ".json");
+                    break;
+
+                default:
+                    Console.WriteLine("No activity selected");
+                    break;
+            }
+            /*string user;
+            user = Console.ReadLine();
+            StatsContext.ExportMovies("stats.csv", "metadata.csv", user + ".json");*/
+
         }
     }
 }
